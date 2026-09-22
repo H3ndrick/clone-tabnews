5914043670867ad9563a768bd97891783cda1687
Hendrick Ascendino <88857648+H3ndrick@users.noreply.github.com>
2026-09-22 03:58:35 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Se você não gosta do seu destino, não o aceite. Em vez disso, tenha a coragem para transformá-lo naquilo que você quer que ele seja.</h1>
+}
+
+export default Home;
